f15ad4dd83ba7c577fc6ef2815d55a649ced9a58
Vajkis <vajkis1@gmail.com>
2026-07-31 00:40:14 +0300

    v1.0.1 fix actions icons path

--- data/actions.js
+++ data/actions.js
@@ -1,12 +1,12 @@
 export const ACTIONS = [
-  { id: 'hit-light', label: 'Light Hit', value: -3, img: '/img/_actions/hit_light.png' },
-  { id: 'hit-medium', label: 'Medium Hit', value: -6, img: '/img/_actions/hit_medium.png' },
-  { id: 'hit-heavy', label: 'Heavy Hit', value: -9, img: '/img/_actions/hit_heavy.png' },
-  { id: 'draw', label: 'Draw', value: -15, img: '/img/_actions/draw.png' },
-  { id: 'punch', label: 'Punch', value: 2, img: '/img/_actions/punch.png' },
-  { id: 'bend', label: 'Bend', value: 7, img: '/img/_actions/bend.png' },
-  { id: 'upset', label: 'Upset', value: 13, img: '/img/_actions/upset.png' },
-  { id: 'shrink', label: 'Shrink', value: 16, img: '/img/_actions/shrink.png' }
+  { id: 'hit-light', label: 'Light Hit', value: -3, img: 'img/_actions/hit_light.png' },
+  { id: 'hit-medium', label: 'Medium Hit', value: -6, img: 'img/_actions/hit_medium.png' },
+  { id: 'hit-heavy', label: 'Heavy Hit', value: -9, img: 'img/_actions/hit_heavy.png' },
+  { id: 'draw', label: 'Draw', value: -15, img: 'img/_actions/draw.png' },
+  { id: 'punch', label: 'Punch', value: 2, img: 'img/_actions/punch.png' },
+  { id: 'bend', label: 'Bend', value: 7, img: 'img/_actions/bend.png' },
+  { id: 'upset', label: 'Upset', value: 13, img: 'img/_actions/upset.png' },
+  { id: 'shrink', label: 'Shrink', value: 16, img: 'img/_actions/shrink.png' }
 ];
 
 export function getActionById(id) {
